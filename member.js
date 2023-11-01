a58de04c89ada12e8f80be5ef589322d7c079855
Oct Oscar <71621912+6spiritman@users.noreply.github.com>
2023-11-01 17:48:15 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillMember() {
+    // Path: member.js
+    var member = {
+        name: 'Member',
+        url: '/member',
+        views: {
+            'main@': {
+                templateUrl: 'member/member.html',
+                controller: 'MemberController',
+                controllerAs: 'vm'
+            }
+        }
+    };
+
+    return member;
+}
